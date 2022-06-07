afaa7d2981a3ada9bd1afb1ec8726d5b7e0700fa
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-06-07 23:45:10 +0300

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return 'This one is on me!'
+  }
+  else if (distance > 2000 && distance < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (distance > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city === "NYC") ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipString){
   // Write your code here!
+  if (tipString === "generous") {
+    return "Thank you so much."
+  }
+  else if (tipString === "not as generous") {
+    return "Thank you."
+  }
+  else if (tipString === "thanks for everything") {
+    return "Bye."
+  }
 }
